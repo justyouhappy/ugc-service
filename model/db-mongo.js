0c1a8da9edf6e025b4f977e4dc65cacf86124de5
yuhuajian.alan <yuhuajian.alan@bytedance.com>
2018-04-27 11:43:40 +0800

    修改moogose配置

--- model/db-mongo.js
+++ model/db-mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');    //引用mongoose模块
-mongoose.connect('mongodb://localhost/text'); //创建一个数据库连接
+mongoose.connect('mongodb://localhost:54321/yuhuajian'); //创建一个数据库连接
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 module.exporrts = db;
